2e859bbc1792c0f2066b535f5e8c32122c53ff57
Alexandre Dubeau <adubeau@c3r9p6.42quebec.com>
2022-12-01 13:53:41 -0500

    day1 python done

---
+++ 1/day1.py
@@ -0,0 +1,32 @@
+total = 0
+max = 0
+result = []
+cur = []
+
+with open("data", "r") as fd:
+    lines = fd.readlines()
+
+for line in lines:
+    if line == "\n":
+        result.append(cur)
+        cur = []
+    else:
+        cur.append(int(line.replace("\n", "")))
+
+max = sum(result[0])
+
+for i in result:
+    total = sum(i)
+    if total > max:
+        max = total
+
+top3 = [0,0,0]
+for i in result:
+    total = sum(i)
+    if total > min(top3):
+        top3.remove(min(top3))
+        top3.append(total)
+
+print(sum(top3))
+
+print(max)
