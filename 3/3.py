c647d26d6ba5a2ca78a9d400ecd69231801ef942
Alexandre Dubeau <adubeau@c3r6p6.42quebec.com>
2022-12-03 14:15:17 -0500

    added total1 for part2

--- 3/3.py
+++ 3/3.py
@@ -6,6 +6,8 @@ def get_value(key):
 
 
 total = 0
+total1 = 0
+i = 0
 key = 'a'
 with open("data", "r") as fd:
     lines = fd.readlines()
@@ -16,5 +18,10 @@ for line in lines:
     b = line[m:]
     key = list(set(a).intersection(b))[0]
     total = total + get_value(key)
+    if i % 3 == 0:
+        set1 = set(set(lines[i].strip()).intersection(lines[i+1].strip()))
+        key = list(set1.intersection(lines[i + 2].strip()))[0]
+        total1 = total1 + get_value(key)
+    i = i + 1
 
-print(total)
+print(total, total1)
