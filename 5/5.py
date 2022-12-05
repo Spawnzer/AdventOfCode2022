3567597dfe06aadfc8ee5a8100437cb5e79ba1ea
Alexandre Dubeau <adubeau@c3r9p6.42quebec.com>
2022-12-05 16:37:58 -0500

    parsing done

---
+++ 5/5.py
@@ -0,0 +1,16 @@
+with open("starting", "r") as fd:
+    lines = fd.readlines()
+
+b = [[0 for x in range(9)] for y in range(9)]
+i = 1
+while i < len(lines):
+    j = 0
+    while j < len(lines[i]):
+        a = lines[i][j]
+        if a >= 'A' and a <= 'Z':
+            b[j//4][i-1] = a
+        j = j + 1
+    i = i + 1
+for line in b:
+    line = [i for i in line if i != 0]
+    print(line)
