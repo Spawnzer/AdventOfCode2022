c8af453b9e8361c544e3e38299d33b51ed8c66f2
Alexandre Dubeau <adubeau@c3r9p6.42quebec.com>
2022-12-05 20:08:40 -0500

    almost there

--- 5/5.py
+++ 5/5.py
@@ -2,6 +2,7 @@ with open("starting", "r") as fd:
     lines = fd.readlines()
 
 b = [[0 for x in range(9)] for y in range(9)]
+
 i = 1
 while i < len(lines):
     j = 0
@@ -11,6 +12,11 @@ while i < len(lines):
             b[j//4][i-1] = a
         j = j + 1
     i = i + 1
+
 for line in b:
     line = [i for i in line if i != 0]
-    print(line)
+    line.reverse()
+
+
+#res.append(line[len(line) - 1])
+print(''.join(res))
